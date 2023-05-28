f9414c04b23a03fc9cc97c50ed0acd7d6f7c87f4
DheerajKamble98 <www.breakkebaad@gmail.com>
2023-05-28 18:31:35 +0530

    container

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, options) {
+    const { webpack } = options;
+    Object.assign(config.experiments, { topLevelAwait: true });
+    if (!options.isServer) {
+      //config.cache=false
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'home',
+          remotes: {
+            home: `home@http://localhost:3000/_next/static/chunks/remoteEntry.js`,
+            reportTerminatedParticipant: 'reportTerminatedParticipant@http://localhost:3001/_next/static/chunks/remoteEntry.js',
+            reportLoginActivity: 'reportLoginActivity@http://localhost:3002/_next/static/chunks/remoteEntry.js',
+          },
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {
+            './pages-map': './pages-map.tsx',
+          },
+          shared: {},
+        }),
+      );
+    }
+
+    return config;
+  },
+};
---
+++ pages/remote.d.ts
@@ -0,0 +1,9 @@
+declare module "reportLoginActivity/reportLoginActivity" {
+  const reportLoginActivity: React.ComponentType;
+  export default reportLoginActivity;
+}
+
+declare module "reportTerminatedParticipant/reportTerminatedParticipant" {
+  const reportTerminatedParticipant: React.ComponentType;
+  export default reportTerminatedParticipant;
+}
